915683afcebbb3e8b01422efb28d0deebb399b6d
khalsel <khaled.sellami.1@ulaval.ca>
2024-04-30 20:43:36 -0400

    updated setup file and versioning

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 # setup.py
+import os
+import re
 from setuptools import setup, find_packages
-
-from msextractor import __version__
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+with open(os.path.join(os.path.dirname(__file__), "msextractor", "_version.py")) as f:
+    version_file = f.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        __version__ = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 if __name__ == "__main__":
     setup(
